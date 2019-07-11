e6c04d5f55ac1ce57b368d8960c0dfde4c7c02eb
Egor Egorov <egigoka@gmail.com>
2019-07-11 18:42:02 +0300

    updating documentation for commands to v 9.0.0-alpha386

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,4 +1,20 @@
 
+
+var cnt = 0;
 function helloWorld(){
     document.write("Hello World!");
+    cnt++;
+    document.body.innerHTML = "<button onclick=\"helloWorld()\">Oh hello</button>" + cnt;
+}
+
+function fillTable(data) {
+    let table = document.querySelector("table");
+    for (let element of data) {
+        let row = table.insertRow();
+        for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
 }
